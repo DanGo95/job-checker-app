88b291e608ea137338295428d7160439ca2f0f9f
Daniel Gómez <danielgomezv2@gmail.com>
2022-12-15 12:52:52 -0300

    angular/fire added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'job-checker-app',
+    appId: '1:695068899118:web:648cbde6b3001c33b4acbf',
+    storageBucket: 'job-checker-app.appspot.com',
+    apiKey: 'AIzaSyA-jSjRo90ayAf8z3s4UX9EW6iHvsdIG0Y',
+    authDomain: 'job-checker-app.firebaseapp.com',
+    messagingSenderId: '695068899118',
+  },
   production: false
 };
 
