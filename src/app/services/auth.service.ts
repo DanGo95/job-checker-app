8202637a27919f0cec2ca8fbef71915e87b9ed81
Daniel Gómez <danielgomezv2@gmail.com>
2022-12-16 12:58:33 -0300

    new methods added to auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  user: any = {};
+
+  constructor( public auth: AngularFireAuth ) {
+    this.auth.authState.subscribe( user => {
+      if (!user) {return;}
+      this.user.photo = user.photoURL;
+      this.user.name = user.displayName;
+      this.user.uid = user.uid;
+    })
+  }
+
+  login() {
+    return this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.user = {};
+    return this.auth.signOut();
+  }
 }
